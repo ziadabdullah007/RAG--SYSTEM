016929daf5d81865a9bd68898216b576bf59bf6f
ziadabdullah007 <zyadabdallah11@gmail.com>
2026-04-18 23:14:26 +0200

    update main

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
+from routes import base 
+
 app = FastAPI()
+app.include_router(base.base_router)
 
-@app.get("/welcom")
 
-def welcom():
-    return{
-    "message" : "444hello world!" 
-}
---
+++ routes/base.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI , APIRouter
+base_router = APIRouter(
+
+prefix = "/api/v1", ## to make it easy to change in future all api 
+tags = ["api_v1"],
+
+)
+
+@base_router.get("/")
+
+async def welcom(): ## async help in future to improve performance 
+    
+    return{
+    "message" : "444hello all!" 
+}
+
